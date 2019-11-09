e78f5c462566bbfac9b8db6c0834e30651d15fdc
Lucas Costa <lucasfrct@users.noreply.github.com>
2019-11-09 19:44:17 -0300

    Add functions de Sorteio

--- app/src/main/java/com/lucas/costa/appsorteio/MainActivity.java
+++ app/src/main/java/com/lucas/costa/appsorteio/MainActivity.java
@@ -2,7 +2,13 @@ package com.lucas.costa.appsorteio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void buttonPlay (View view ) {
+        TextView setNumberView = findViewById( R.id.numberView );
+        TextView nDisplay = findViewById( R.id.numberDisplay );
+
+        int number = this.numberSorted ( );
+
+        setNumberView.setText( "O número selecionado é:" );
+        nDisplay.setText( "" + number + "" );
+    }
+
+    public int numberSorted ( ) {
+        return new Random ( ).nextInt ( 10 );
     }
 }
